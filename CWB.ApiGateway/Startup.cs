0ed85b4f6b078468ac7cab77da14df80b449d648
B Rajugouda Haranhalli <rajugoudaharanahalli@gmail.com>
2024-11-19 14:59:28 +0530

    To Run CI/CD

--- CWB.ApiGateway/Startup.cs
+++ CWB.ApiGateway/Startup.cs
@@ -9,7 +9,7 @@ namespace CWB.ApiGateway
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOcelot();
